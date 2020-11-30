d045b72ca7525e427abd7c7ff6dc9b772670fd49
VICTOR HUGO BENITEZ BOSQUES <vbenitez@bancoazteca.com>
2020-11-30 16:29:01 -0600

    Add xcode project to repo

---
+++ FizzBuzz-Quiz-App/FizzBuzz-Quiz-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FizzBuzz-Quiz-App
+//
+//  Created by Victor Hugo Benitez Bosques on 30/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FizzBuzz-Quiz-App/FizzBuzz-Quiz-AppUITests/FizzBuzz_Quiz_AppUITests.swift
@@ -0,0 +1,12 @@
+//
+//  FizzBuzz_Quiz_AppUITests.swift
+//  FizzBuzz-Quiz-AppUITests
+//
+//  Created by Victor Hugo Benitez Bosques on 30/11/20.
+//
+
+import XCTest
+
+class FizzBuzz_Quiz_AppUITests: XCTestCase {
+
+}
