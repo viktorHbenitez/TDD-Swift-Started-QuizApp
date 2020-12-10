3c91d5cc12b1f0bae9caefff4dc1adccfb513fbf
VICTOR HUGO BENITEZ BOSQUES <vbenitez@bancoazteca.com>
2020-12-10 11:45:05 -0600

    Refactoring Unit Test Case

--- FizzBuzz-Quiz-App/FizzBuzz-Quiz-App/Brain.swift
+++ FizzBuzz-Quiz-App/FizzBuzz-Quiz-App/Brain.swift
@@ -10,6 +10,14 @@ import Foundation
 struct Brain {
     
     func isDivisibleByThree(number: Int) -> Bool{
-        return (number % 3 == 0)
+        return number % 3 == 0
+    }
+    
+    func isDivisibleByFive(number: Int) -> Bool{
+        return number % 5 == 0
+    }
+    
+    func isDivisibleByFifteen(number: Int) -> Bool {
+        return number % 15 == 0
     }
 }
--- FizzBuzz-Quiz-App/FizzBuzz-Quiz-AppTests/BrainTest.swift
+++ FizzBuzz-Quiz-App/FizzBuzz-Quiz-AppTests/BrainTest.swift
@@ -7,12 +7,18 @@
 
 import XCTest
 
+/*
+ Note: The important of TDDis learning to love failing test and warnings
+ */
+
 @testable import FizzBuzz_Quiz_App
 class BrainTest: XCTestCase {
 
-    // Our app can tell if a number is divisible by three
+    // SUT = System Under Test
+    let brain = Brain()
+    
+    // Test Case: Our first Test that our ap can tell if a number is divisible by three
     func testGiveNumberIsDivisibleByThree(){
-        let brain = Brain()  // SUT = System Under Test
 
         // act or when
         let result = brain.isDivisibleByThree(number: 3)
@@ -20,9 +26,35 @@ class BrainTest: XCTestCase {
     }
     
     func testGivenNumberIsNotDivisibleByThree(){
-        let brain = Brain()
-        
         let result = brain.isDivisibleByThree(number: 1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testGivenNumberIsDivisibleByFive(){
+        
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertEqual(result, true)
+        
+    }
+    
+    func testGivenInNotDivisibleByFive(){
+        let result = brain.isDivisibleByFive(number: 3)
+        
+        XCTAssertEqual(result, false)
+        
+        
+    }
+    
+    func testGivenIsDivisibleByFifteen(){
+        
+        let result = brain.isDivisibleByFifteen(number: 15)
+        
+        XCTAssertEqual(result, true)
+        
+    }
+    
+    func testGivenNumberIsDivisibleByFifteen(){
+        let result = brain.isDivisibleByFifteen(number: 3)
         XCTAssertEqual(result, false)
     }
 
